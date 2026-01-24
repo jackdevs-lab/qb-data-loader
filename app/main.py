244d686be1f1c91dbe981a618aaf807e5f6071af
Ndegwa Jackson <128777440+jackdevs-lab@users.noreply.github.com>
2026-01-24 20:53:20 +0300

    Added AR/AP functionalities

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-#from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 from app.core.db import TORTOISE_ORM
 from app.api.jobs import router as jobs_router
@@ -10,7 +9,10 @@ from app.api.mappings import router as mappings_router
 from app.api.reports import router as reports_router
 from app.api.customers import router as customers_router
 from app.api.products import router as products_router
-#rom app.api.ai import router as ai_router  # NEW
+from app.api.invoices import router as invoices_router
+from app.api.sales_receipts import router as sales_receipts_router
+from app.api.expenses import router as expenses_router
+from app.api.references import router as references_router
 from app.api.sync import router as sync_router  # NEW
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
@@ -22,7 +24,10 @@ app.include_router(mappings_router, prefix="/api/mappings")
 app.include_router(reports_router)
 app.include_router(customers_router)
 app.include_router(products_router)
-#app.include_router(ai_router)  # NEW
+app.include_router(invoices_router)
+app.include_router(sales_receipts_router)
+app.include_router(expenses_router)
+app.include_router(references_router)
 app.include_router(sync_router)  # NEW
 app.add_middleware(
     CORSMiddleware,
